745e9f6e0eaa24366a10255d8014f6277ecfaaa7
Akib <jbc.akib@gmail.com>
2025-07-26 22:53:10 +0600

    Remove UserController and update API routes to use AuthController for user authentication

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\PostController;
 
-// Route::post('/register', [UserController::class, 'register']);
-// Route::post('/login', [UserController::class, 'login']);
-// Route::get('/dashboard', [UserController::class, 'dashboard'])->middleware('auth:api');
-// Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:api');
-
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
